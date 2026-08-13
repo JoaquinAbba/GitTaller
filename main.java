9897fa23ed780d8e2f98f7d8e4131cf0e30cc27d
jeremias lucero <ljeremias712@gmail.com>
2026-08-13 17:44:51 -0300

    fijate lo que puse en el main y en el system.out.println

--- main.java
+++ main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        System.out.println("hola joaco soy jere,me adueñe de la compu y te voy a borrar todo");
     }
 }
