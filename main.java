41c5092c2740d8544c5fa955f48b5c33a8dc606c
JoaquinAbba <joaquinabbam@gmail.com>
2026-08-13 16:56:54 -0300

    asd4

--- main.java
+++ main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+        System.out.println("asdsadadasdsadasdsad 2.");
+        System.out.println("asdsadadasdsadasdsad 3.");
+        System.out.println("asdsadadasdsadasdsad 4.");
     }
 }
